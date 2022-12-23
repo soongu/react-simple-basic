cfa0b3bb0567f81f69c5b9f6be4d1102c07c1d89
soongu <hsg9984@gmail.com>
2022-12-23 12:13:44 +0900

    - useEffect #2

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ const App = () => {
     const [userName, setUserName] = useState('Kim');
 
     // 마운트 될 때와 값이 변경될 때 실행
+    // 두번째 인자에 빈배열 넣을시: 처음 렌더링시에만 실행
+    // 두번째 인자에 배열에 변수넣을시: 해당 값이 업데이트될때만 실행
+    // 함수리턴시: clean-up함수 반환하여 컴포너넌트가 언마운트되거나 업데이트되기 직전에 실행
     useEffect(() => {
         console.log('useEffect call!');
         console.log('userName:' + userName);
-    });
+
+        return () => {
+            console.log('cleanup');
+            console.log('cleanup username: ' + userName);
+        };
+
+    }, [userName]);
 
     const handleClickPark = () => setUserName("Park");
     const handleClickHong = () => setUserName("Hong");
