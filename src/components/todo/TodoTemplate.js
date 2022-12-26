7a0656dff987be46fccf12088b53ee67d53587fe
soongu <hsg9984@gmail.com>
2022-12-26 11:09:45 +0900

    - TodoList #3. 컴포넌트 상태 중앙 집중화

--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -4,15 +4,15 @@ import TodoHeader from "./TodoHeader";
 import TodoList from "./TodoList";
 import TodoInput from "./TodoInput";
 
-const TodoTemplate = ({todos, handlers}) => {
+const TodoTemplate = ({todos, value, handlers}) => {
 
-    const {onInsert, onRemove, onToggle} = handlers;
+    const {onInsert, onRemove, onToggle, onChange, onSubmit} = handlers;
 
     return (
         <div className="TodoTemplate">
             <TodoHeader todos={todos} />
             <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-            <TodoInput onInsert={onInsert} />
+            <TodoInput onInsert={onInsert} onChange={onChange} onSubmit={onSubmit} value={value} />
         </div>
     );
 };
